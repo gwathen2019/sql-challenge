3a16b91598c0991286d9c61ee85f6032c6f0a51b
gwathen2019 <gwathen@att.net>
2019-10-09 12:44:33 -0500

    git commit 	modified:   EmployeeSQL/sql_query1.sql

--- EmployeeSQL/sql_query1.sql
+++ EmployeeSQL/sql_query1.sql
@@ -1,6 +1,7 @@
 --Data analysis
 
--- 1.
+-- 1. List the following details of each employee: employee number, last name, first name, gender, and salary.
+
 /*select Employees.emp_no, 
 	Employees.last_name, 
 	Employees.first_name, 
@@ -10,7 +11,8 @@ from Employees
 inner join Salaries on
 Employees.emp_no = Salaries.emp_no;*/
 
--- 2.
+-- 2. List employees who were hired in 1986.
+
 /*select employees.emp_no,
 	employees.first_name,
 	employees.last_name,
@@ -18,7 +20,9 @@ Employees.emp_no = Salaries.emp_no;*/
 from employees
 where hire_date > '12/31/1985' and hire_date < '01/01/1987';*/
 
--- 3.
+-- 3. List the manager of each department with the following information: department number, department name,
+--    the manager's employee number, last name, first name, and start and end employment dates.
+
 /*select t1.dept_no,
 	t3.dept_name,
 	t1.emp_no,
@@ -32,7 +36,9 @@ t1.emp_no = t2.emp_no
 join departments as t3
 on t3.dept_no = t1.dept_no;*/
 
--- 4.
+-- 4. List the department of each employee with the following information: employee number, last name, first name,
+--    and department name.
+
 /*select t1.emp_no,
 	t1.last_name,
 	t1.first_name,
@@ -43,13 +49,16 @@ on t1.emp_no = t2.emp_no
 join departments as t3
 on t3.dept_no = t2.dept_no;*/
 
--- 5.
+-- 5. List all employees whose first name is "Hercules" and last names begin with "B."
+
 /*select employees.first_name,
 	employees.last_name
 from employees
 where first_name = 'Hercules' and last_name like 'B%'; */
 
--- 6.
+-- 6. List all employees in the Sales department, including their employee number, last name,
+--    first name, and department name.
+
 /*select t1.emp_no
 	, t1.last_name
 	, t1.first_name
@@ -61,7 +70,9 @@ join departments as t3
 on t3.dept_no = t2.dept_no
 where t3.dept_name = 'Sales';*/
 
--- 7.
+-- 7. List all employees in the Sales and Development departments, including their employee number,
+--    last name, first name, and department name.
+
 /*select t1.emp_no
 	, t1.last_name
 	, t1.first_name
@@ -73,7 +84,9 @@ join departments as t3
 on t3.dept_no = t2.dept_no
 where t3.dept_name = 'Sales' or t3.dept_name = 'Development';*/
 
--- 8.
+-- 8. In descending order, list the frequency count of employee last names, i.e., how many employees
+--    share each last name.
+
 /*select last_name, count (last_name) as name_cnt
 from employees
 group by last_name
